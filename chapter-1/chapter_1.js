c47288a57fa53a3cdb4b9866827351cc625535ce
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-05-06 10:20:31 +0530

    datatypes-chapter-1

--- chapter-1/chapter_1.js
+++ chapter-1/chapter_1.js
@@ -9,7 +9,6 @@ var title = "Let's Learn Javascript⚡";
 // title = "Javascript🔥"
 // console.log(title);
 
-
 // *2. let ( new & modern way)
 
 let isDone = "Yes✅";
@@ -17,9 +16,8 @@ let isDone = "Yes✅";
 // let isDone = "No❌"
 
 // re-assign
-isDone = "No🔥"
+isDone = "No🔥";
 console.log(isDone);
-
 
 // *3. Const ( new & modern way)
 
@@ -27,4 +25,56 @@ const PI = 3.14;
 // const PI = 22/7
 
 // PI = 12039120;
-console.log(PI)
+console.log(PI);
+
+// Primitive Data Types ( )
+
+// *1. String
+let username = 'Suraj , "hello" '; //"SurajKumarJha"
+let para = `Hello world
+this is suraj
+what is going here
+kya haal chaal hai
+`;
+
+console.log(para);
+
+// *2. Number
+let age = 20;
+console.log(age);
+// *3. Boolean
+let isProgrammer = true;
+console.log(isProgrammer);
+
+// *4. null
+let empty = null;
+let college;
+// *5. undefined
+// ?6. symbols
+// ?7. BigInt
+
+
+// *2. Non Primitive Data types
+
+// *1. Array
+let fruits = ["🍇" , "🍊" , "🍐"]
+
+console.log(fruits)
+
+// *2. Object
+
+let Person = {
+    name:"Suraj",
+    age:20,
+    isProgrammer:true
+}
+
+console.log(Person)
+
+// *3. Function
+
+function add(){
+    console.log(10+20)
+}
+
+add()

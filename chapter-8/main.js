926f0cca062a681205dd069ee96329eb65ed9ceb
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-05-10 13:17:55 +0530

    chapter-7-8

---
+++ chapter-8/main.js
@@ -0,0 +1,91 @@
+let Num = [90,12,1,0,23423,1234];
+
+// *join
+
+let data = Num.join("+")
+console.log(data)
+
+// *indexOf
+// let res = Num.indexOf(0.1)
+
+// console.log(res)
+
+// * includes
+
+// let Num2 = [12,12,321,4,4,5,56,6]
+
+// let transport = ["car" , "bike" , "train" , "airplane"];
+
+// const data = transport.includes("cycle")
+
+// console.log(data)
+
+// *Concat
+// let num3 = Num.concat(Num2)
+
+// console.log(num3)
+
+// *Slice Method
+
+// let data = Num.slice(0,3)
+
+// console.log(data)
+
+
+// Num.reverse()
+
+// console.log(Num)
+
+
+// *sort
+// Num.sort()
+
+// console.log(Num)
+
+
+// * splice (remove , replace , insert)
+
+// !. Remove
+
+// Num.splice(0 , 3)
+
+// ?. Replace elements
+
+// Num.splice(0 , 2 , 89,90)
+// console.log(Num)
+
+// *Adding Elements
+
+// Num.splice(0 , 0 , 12,13)
+// console.log(Num)
+
+// Num.splice(3)
+
+//  console.log(Num)
+// console.log(Num)
+// * unshift
+
+// Num.unshift(12,34,5,6,7)
+// console.log(Num)
+
+// * Shift
+// let res = Num.shift()
+
+// console.log(res)
+// console.log(Num)
+
+
+
+// * Pop
+
+// let res = Num.pop()
+
+// console.log(res)
+// console.log(Num)
+
+
+// *. Push
+
+// Num.push(8,"Suraj" , true)
+
+
